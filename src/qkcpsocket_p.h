25bd549ea84978ade7fe08819ee1c3fec45f6c56
mengps <843261040@qq.com>
2023-02-16 17:10:51 +0800

    Update: ikcp_input & ikcp_send 立即 update.

--- src/qkcpsocket_p.h
+++ src/qkcpsocket_p.h
@@ -6,6 +6,7 @@
 
 #include <QDateTime>
 #include <QUdpSocket>
+#include <QBasicTimer>
 
 class QKcpSocketPrivate
 {
@@ -19,6 +20,7 @@ public:
         QKcpSocketPrivate *_this = reinterpret_cast<QKcpSocketPrivate *>(user);
 
         int ret = _this->m_udpSocket->writeDatagram(buf, len, _this->m_connectionAddress, _this->m_connectionPort);
+
         if (ret < 0) {
             if (_this->m_isConnceted) {
                 _this->m_isConnceted = false;
@@ -32,9 +34,26 @@ public:
     void _release_kcp()
     {
         if (m_kcpContext) {
-            ikcp_release(m_kcpContext);
+            auto releaseCtx = m_kcpContext;
             m_kcpContext = nullptr;
+            ikcp_release(releaseCtx);
         }
+    }
+
+    int _kcp_input(const char *data, long size)
+    {
+        int ret = ikcp_input(m_kcpContext, data, size);
+        _kcp_update();
+
+        return ret;
+    }
+
+    int _kcp_send(const char *data, long size)
+    {
+        int ret = ikcp_send(m_kcpContext, data, size);
+        _kcp_update();
+
+        return ret;
     }
 
     void _kcp_update()
@@ -44,17 +63,17 @@ public:
         if (!m_kcpContext) return;
 
         auto cur_time = QDateTime::currentMSecsSinceEpoch();
+        ikcp_update(m_kcpContext, cur_time);
+        int peek_size = ikcp_peeksize(m_kcpContext);
+        if (peek_size > 0) {
+            m_bytesAvailable = peek_size;
+            emit q->QIODevice::readyRead();
+        }
+
         if ((cur_time - m_lastRecvTime) > KCP_CONNECTION_TIMEOUT_TIME) {
             if (m_isConnceted) {
                 m_isConnceted = false;
                 emit q->disconnected();
-            }
-        } else {
-            ikcp_update(m_kcpContext, cur_time);
-            int peek_size = ikcp_peeksize(m_kcpContext);
-            if (peek_size > 0) {
-                m_bytesAvailable = peek_size;
-                emit q->QIODevice::readyRead();
             }
         }
     }
@@ -69,9 +88,10 @@ public:
     quint16 m_hostPort, m_connectionPort;
     ikcpcb *m_kcpContext = nullptr;
     quint32 m_kcpConv = 0;
+    quint32 m_kcpMaxSendSize = 0;
     QUdpSocket *m_udpSocket = nullptr;
+    QBasicTimer m_updateTimer;
     quint64 m_lastRecvTime;
-    QTimer *m_updateTimer = nullptr;
     QKcpSocket::Mode m_mode = QKcpSocket::Mode::Default;
 };
 
