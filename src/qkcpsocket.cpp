5ccb9422f763f720b6e21b3d0252fbacdb459aba
mengps <843261040@qq.com>
2023-02-16 15:54:32 +0800

    Update: 1.Kcp 内部限制了一次发送的大小, 需要手动分包; 2.使用 QBasicTimer 代替 QTimer.

--- src/qkcpsocket.cpp
+++ src/qkcpsocket.cpp
@@ -2,7 +2,7 @@
 
 #include <QDateTime>
 #include <QNetworkDatagram>
-#include <QTimer>
+#include <QBasicTimer>
 
 static quint32 get_new_conv()
 {
@@ -19,9 +19,6 @@ QKcpSocket::QKcpSocket(Mode mode, QObject *parent)
     d->m_mode = mode;
     d->m_kcpConv = get_new_conv();
     d->m_udpSocket = new QUdpSocket(this);
-    d->m_updateTimer = new QTimer(this);
-
-    connect(d->m_updateTimer, SIGNAL(timeout()), this, SLOT(_kcp_update()));
 }
 
 QKcpSocket::~QKcpSocket()
@@ -45,9 +42,6 @@ void QKcpSocket::connectToHost(const QHostAddress &address, qint16 port)
     d->m_isBinded = d->m_udpSocket->bind();
 
     if (d->m_isBinded) {
-        d->m_udpSocket->open(QIODevice::ReadWrite);
-        d->m_updateTimer->start(10);
-
         d->m_kcpContext = ikcp_create(d->m_kcpConv, d);
         d->m_kcpContext->output = QKcpSocketPrivate::udp_output;
         /*
@@ -65,22 +59,23 @@ void QKcpSocket::connectToHost(const QHostAddress &address, qint16 port)
                 | IKCP_LOG_OUT_PROBE
                 | IKCP_LOG_OUT_WINS;
         */
-        ikcp_wndsize(d->m_kcpContext, 32, 32);
-        //ikcp_setmtu(d->m_kcpContext, 256);
+        ikcp_wndsize(d->m_kcpContext, 128, 128);
+        ikcp_setmtu(d->m_kcpContext, 1400);
+        d->m_kcpMaxSendSize = d->m_kcpContext->mtu * (d->m_kcpContext->snd_wnd - 4);
 
         if (d->m_mode == Mode::Default) {
-            ikcp_nodelay(d->m_kcpContext, 0, 10, 0, 0); //default mode
+            ikcp_nodelay(d->m_kcpContext, 0, 10, 0, 0);
         } else if (d->m_mode == Mode::Normal) {
-            ikcp_nodelay(d->m_kcpContext, 0, 10, 0, 1); //normal mode, close flow-control
+            ikcp_nodelay(d->m_kcpContext, 0, 10, 0, 1);
         } else {
             /**
              * Fast mode
-             * 第二个参数 nodelay-启用以后若干常规加速将启动
-             * 第三个参数 interval为内部处理时钟，默认设置为 10ms
-             * 第四个参数 resend为快速重传指标，设置为2
-             * 第五个参数 为是否禁用常规流控，这里禁止
+             * 第二个参数 nodelay 启用以后若干常规加速将启动
+             * 第三个参数 interval 为内部处理时钟，默认设置为 10ms
+             * 第四个参数 resend 为快速重传指标，设置为2
+             * 第五个参数 nc 为是否禁用常规流控，这里禁止
              */
-            ikcp_nodelay(d->m_kcpContext, 1, 10, 2, 1);
+            ikcp_nodelay(d->m_kcpContext, 0, 10, 2, 1);
             d->m_kcpContext->rx_minrto = 10;
             d->m_kcpContext->fastresend = 1;
         }
@@ -102,9 +97,13 @@ void QKcpSocket::connectToHost(const QHostAddress &address, qint16 port)
                 }
                 const auto data = datagram.data();
                 d->m_lastRecvTime = QDateTime::currentMSecsSinceEpoch();
-                ikcp_input(d->m_kcpContext, data.constData(), data.size());
+                d->_kcp_input(data.constData(), data.size());
             }
         });
+
+        QIODevice::open(QIODevice::ReadWrite);
+        d->m_udpSocket->open(QIODevice::ReadWrite);
+        d->m_updateTimer.start(10, this);
     }
 }
 
@@ -113,9 +112,10 @@ void QKcpSocket::disconnectToHost()
     Q_D(QKcpSocket);
 
     if (d->m_isBinded) {
-        d->m_updateTimer->stop();
+        QIODevice::close();
         d->m_udpSocket->abort();
         d->_release_kcp();
+        d->m_updateTimer.stop();
         disconnect(d->m_udpSocket, &QUdpSocket::readyRead, nullptr, nullptr);
 
         if (d->m_isConnceted) {
@@ -123,6 +123,20 @@ void QKcpSocket::disconnectToHost()
             emit disconnected();
         }
     }
+}
+
+QHostAddress QKcpSocket::peerAddress() const
+{
+    Q_D(const QKcpSocket);
+
+    return d->m_connectionAddress;
+}
+
+quint16 QKcpSocket::peerPort() const
+{
+    Q_D(const QKcpSocket);
+
+    return d->m_connectionPort;
 }
 
 bool QKcpSocket::isConnected() const
@@ -139,6 +153,15 @@ void *QKcpSocket::kcpHandle() const
     return d->m_kcpContext;
 }
 
+int QKcpSocket::waitSentCount() const
+{
+    Q_D(const QKcpSocket);
+
+    if (d->m_kcpContext)
+        return ikcp_waitsnd(d->m_kcpContext);
+    else return 0;
+}
+
 qint64 QKcpSocket::bytesAvailable() const
 {
     Q_D(const QKcpSocket);
@@ -150,14 +173,40 @@ qint64 QKcpSocket::readData(char *data, qint64 maxlen)
 {
     Q_D(QKcpSocket);
 
-    return ikcp_recv(d->m_kcpContext, data, maxlen);
+    int ret = ikcp_recv(d->m_kcpContext, data, maxlen);
+    if (ret > 0)
+        d->m_bytesAvailable -= ret;
+
+    return ret;
 }
 
 qint64 QKcpSocket::writeData(const char *data, qint64 len)
 {
     Q_D(QKcpSocket);
 
-    return ikcp_send(d->m_kcpContext, data, len) == 0 ? len : -1;
+    int ret;
+    if (len > d->m_kcpMaxSendSize) {
+        /*! KCP 内部限制一次了可发送最大大小, 需要分包 */
+        int count = len / d->m_kcpMaxSendSize;
+        for (int i = 0; i < count; i++) {
+            ret = d->_kcp_send(data + i * d->m_kcpMaxSendSize, d->m_kcpMaxSendSize);
+            if (ret != 0)
+                return ret;
+        }
+        ret = d->_kcp_send(data + count * d->m_kcpMaxSendSize, len - count * d->m_kcpMaxSendSize);
+    } else {
+        ret = d->_kcp_send(data, len);
+    }
+
+    return ret == 0 ? len : ret;
+}
+
+void QKcpSocket::timerEvent(QTimerEvent *event)
+{
+    Q_UNUSED(event);
+    Q_D(QKcpSocket);
+
+    d->_kcp_update();
 }
 
 #include "moc_qkcpsocket.cpp"
