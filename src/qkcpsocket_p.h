591d01b695d9cdff7597b969ef87968eb3fcfd17
mengps <843261040@qq.com>
2023-02-23 20:02:57 +0800

    Fix: 1.sendto返回-1不应作为断连处理; 2.部分代码移动至此文件.

--- src/qkcpsocket_p.h
+++ src/qkcpsocket_p.h
@@ -4,9 +4,10 @@
 #include "ikcp.h"
 #include "qkcpsocket.h"
 
-#include <QDateTime>
-#include <QUdpSocket>
 #include <QBasicTimer>
+#include <QDateTime>
+#include <QNetworkDatagram>
+#include <QUdpSocket>
 
 class QKcpSocketPrivate
 {
@@ -17,18 +18,63 @@ public:
 
     static int udp_output(const char *buf, int len, struct IKCPCB *kcp, void *user)
     {
+        Q_UNUSED(kcp);
+
         QKcpSocketPrivate *_this = reinterpret_cast<QKcpSocketPrivate *>(user);
 
         int ret = _this->m_udpSocket->writeDatagram(buf, len, _this->m_connectionAddress, _this->m_connectionPort);
 
+        /*! Windows ret 有时会为 -1, 但不能作为错误处理 */
         if (ret < 0) {
-            if (_this->m_isConnceted) {
+            /*if (_this->m_isConnceted) {
                 _this->m_isConnceted = false;
                 emit _this->q_ptr->disconnected();
-            }
+            }*/
         }
 
         return ret;
+    }
+
+    void _create_kcp()
+    {
+        m_kcpContext = ikcp_create(m_kcpConv, this);
+        m_kcpContext->output = QKcpSocketPrivate::udp_output;
+        //m_kcpContext->writelog = kcp_log;
+        m_kcpContext->logmask = IKCP_LOG_OUTPUT
+                | IKCP_LOG_INPUT
+                | IKCP_LOG_SEND
+                | IKCP_LOG_RECV
+                | IKCP_LOG_IN_DATA
+                | IKCP_LOG_IN_ACK
+                | IKCP_LOG_IN_PROBE
+                | IKCP_LOG_IN_WINS
+                | IKCP_LOG_OUT_DATA
+                | IKCP_LOG_OUT_ACK
+                | IKCP_LOG_OUT_PROBE
+                | IKCP_LOG_OUT_WINS;
+
+        ikcp_wndsize(m_kcpContext, 128, 128);
+
+        ikcp_setmtu(m_kcpContext, 1400);
+
+        m_kcpMaxSendSize = m_kcpContext->mtu * (m_kcpContext->snd_wnd - 4);
+
+        if (m_mode == QKcpSocket::Mode::Default) {
+            ikcp_nodelay(m_kcpContext, 0, 10, 0, 0);
+        } else if (m_mode == QKcpSocket::Mode::Normal) {
+            ikcp_nodelay(m_kcpContext, 0, 10, 0, 1);
+        } else {
+            /**
+             * Fast mode
+             * 第二个参数 nodelay 启用以后若干常规加速将启动
+             * 第三个参数 interval 为内部处理时钟，默认设置为 10ms
+             * 第四个参数 resend 为快速重传指标，设置为2
+             * 第五个参数 nc 为是否禁用常规流控，这里禁止
+             */
+            ikcp_nodelay(m_kcpContext, 0, 10, 2, 1);
+            m_kcpContext->rx_minrto = 10;
+            m_kcpContext->fastresend = 1;
+        }
     }
 
     void _release_kcp()
@@ -78,6 +124,28 @@ public:
         }
     }
 
+    void _read_udp_data()
+    {
+        Q_Q(QKcpSocket);
+
+        while (m_udpSocket->hasPendingDatagrams()) {
+            const auto datagram = m_udpSocket->receiveDatagram();
+            if (!datagram.isValid()) return;
+
+            /*! 连接后需更改源地址端口 */
+            if (m_firstChange) {
+                m_connectionAddress = datagram.senderAddress();
+                m_connectionPort = datagram.senderPort();
+                m_firstChange = false;
+                m_isConnceted = true;
+                emit q->connected();
+            }
+            const auto data = datagram.data();
+            m_lastRecvTime = QDateTime::currentMSecsSinceEpoch();
+            _kcp_input(data.constData(), data.size());
+        }
+    }
+
     QKcpSocket *q_ptr = nullptr;
 
     quint64 m_bytesAvailable = 0;
@@ -87,8 +155,8 @@ public:
     QHostAddress m_hostAddress, m_connectionAddress;
     quint16 m_hostPort, m_connectionPort;
     ikcpcb *m_kcpContext = nullptr;
-    quint32 m_kcpConv = 0;
     quint32 m_kcpMaxSendSize = 0;
+    quint32 m_kcpConv = 1000;
     QUdpSocket *m_udpSocket = nullptr;
     QBasicTimer m_updateTimer;
     quint64 m_lastRecvTime;
