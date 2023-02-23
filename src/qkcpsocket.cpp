57c2252a0a5defc8bbef0e248c1bf28b6848e7c9
mengps <843261040@qq.com>
2023-02-23 19:59:24 +0800

    Fix: 1.断开连接后老的 KCP 上下文应该重新分配(因为永远都不会收到确认); 2.添加 Server 端专有构造.

--- src/qkcpsocket.cpp
+++ src/qkcpsocket.cpp
@@ -1,123 +1,95 @@
 #include "qkcpsocket_p.h"
 
-#include <QDateTime>
-#include <QNetworkDatagram>
-#include <QBasicTimer>
-
-static quint32 get_new_conv()
-{
-    static uint32_t static_cur_conv = 1000;
-    return ++static_cur_conv;
-}
-
-QKcpSocket::QKcpSocket(Mode mode, QObject *parent)
+QKcpSocket::QKcpSocket(Mode mode, quint32 kcpConv, QObject *parent)
     : QIODevice(parent)
     , d_ptr(new QKcpSocketPrivate(this))
 {
     Q_D(QKcpSocket);
 
     d->m_mode = mode;
-    d->m_kcpConv = get_new_conv();
+    d->m_kcpConv = kcpConv;
     d->m_udpSocket = new QUdpSocket(this);
+
+    d->_create_kcp();
+
+    connect(d->m_udpSocket, SIGNAL(readyRead()), this, SLOT(_read_udp_data()));
+
+    d->m_updateTimer.start(10, this);
+}
+
+/*! QKcpServer 使用 */
+QKcpSocket::QKcpSocket(quint32 kcpConv, const QHostAddress &address, quint16 port)
+    : QIODevice(nullptr)
+    , d_ptr(new QKcpSocketPrivate(this))
+{
+    Q_D(QKcpSocket);
+
+    d->m_mode = Mode::Fast;
+    d->m_kcpConv = kcpConv;
+    d->m_udpSocket = new QUdpSocket(this);
+    d->_create_kcp();
+
+    connect(d->m_udpSocket, &QUdpSocket::readyRead, this, [d]{
+        while (d->m_udpSocket->hasPendingDatagrams()) {
+            const auto datagram = d->m_udpSocket->receiveDatagram();
+            if (!datagram.isValid()) return;
+            const auto data = datagram.data();
+            d->m_lastRecvTime = QDateTime::currentMSecsSinceEpoch();
+            d->_kcp_input(data.constData(), data.size());
+        }
+    });
+
+    connectToHost(address, port);
+
+    d->m_isConnceted = true;
 }
 
 QKcpSocket::~QKcpSocket()
 {
     Q_D(QKcpSocket);
 
-    d->m_udpSocket->close();
+    if (d->m_updateTimer.isActive())  d->m_updateTimer.stop();
+
+    disconnectFromHost();
+
     d->_release_kcp();
+
+    emit deleteFromHost(d->m_connectionAddress, d->m_connectionPort, QPrivateSignal{});
 }
 
 void QKcpSocket::connectToHost(const QHostAddress &address, quint16 port)
 {
     Q_D(QKcpSocket);
 
-    disconnectToHost();
+    QIODevice::close();
+    d->m_udpSocket->close();
+
+    d->_release_kcp();
 
     d->m_hostAddress = d->m_connectionAddress = address;
     d->m_hostPort = d->m_connectionPort = port;
     d->m_firstChange = true;
+    d->m_isConnceted = false;
 
-    d->m_isBinded = d->m_udpSocket->bind(QHostAddress::Any);
+    d->m_isBinded = d->m_udpSocket->bind(QHostAddress::Any, 0, QAbstractSocket::ShareAddress | QAbstractSocket::ReuseAddressHint);
 
     if (d->m_isBinded) {
-        d->m_kcpContext = ikcp_create(d->m_kcpConv, d);
-        d->m_kcpContext->output = QKcpSocketPrivate::udp_output;
-        //d->m_kcpContext->writelog = kcp_log;
-        d->m_kcpContext->logmask = IKCP_LOG_OUTPUT
-                | IKCP_LOG_INPUT
-                | IKCP_LOG_SEND
-                | IKCP_LOG_RECV
-                | IKCP_LOG_IN_DATA
-                | IKCP_LOG_IN_ACK
-                | IKCP_LOG_IN_PROBE
-                | IKCP_LOG_IN_WINS
-                | IKCP_LOG_OUT_DATA
-                | IKCP_LOG_OUT_ACK
-                | IKCP_LOG_OUT_PROBE
-                | IKCP_LOG_OUT_WINS;
-
-        ikcp_wndsize(d->m_kcpContext, 128, 128);
-
-        ikcp_setmtu(d->m_kcpContext, 1400);
-
-        d->m_kcpMaxSendSize = d->m_kcpContext->mtu * (d->m_kcpContext->snd_wnd - 4);
-
-        if (d->m_mode == Mode::Default) {
-            ikcp_nodelay(d->m_kcpContext, 0, 10, 0, 0);
-        } else if (d->m_mode == Mode::Normal) {
-            ikcp_nodelay(d->m_kcpContext, 0, 10, 0, 1);
-        } else {
-            /**
-             * Fast mode
-             * 第二个参数 nodelay 启用以后若干常规加速将启动
-             * 第三个参数 interval 为内部处理时钟，默认设置为 10ms
-             * 第四个参数 resend 为快速重传指标，设置为2
-             * 第五个参数 nc 为是否禁用常规流控，这里禁止
-             */
-            ikcp_nodelay(d->m_kcpContext, 0, 10, 2, 1);
-            d->m_kcpContext->rx_minrto = 10;
-            d->m_kcpContext->fastresend = 1;
-        }
+        QIODevice::open(QIODevice::ReadWrite);
         d->m_lastRecvTime = QDateTime::currentMSecsSinceEpoch();
 
-        connect(d->m_udpSocket, &QUdpSocket::readyRead, this, [this]{
-            Q_D(QKcpSocket);
-            while (d->m_udpSocket->hasPendingDatagrams()) {
-                const auto datagram = d->m_udpSocket->receiveDatagram();
-                if (!datagram.isValid()) return;
-
-                /*! 连接后需更改源地址端口 */
-                if (d->m_firstChange) {
-                    d->m_connectionAddress = datagram.senderAddress();
-                    d->m_connectionPort = datagram.senderPort();
-                    d->m_firstChange = false;
-                    d->m_isConnceted = true;
-                    emit connected();
-                }
-                const auto data = datagram.data();
-                d->m_lastRecvTime = QDateTime::currentMSecsSinceEpoch();
-                d->_kcp_input(data.constData(), data.size());
-            }
-        });
-
-        QIODevice::open(QIODevice::ReadWrite);
-        d->m_udpSocket->open(QIODevice::ReadWrite);
-        d->m_updateTimer.start(10, this);
+        d->_create_kcp();
     }
 }
 
-void QKcpSocket::disconnectToHost()
+void QKcpSocket::disconnectFromHost()
 {
     Q_D(QKcpSocket);
 
     if (d->m_isBinded) {
+        d->m_isBinded = false;
         QIODevice::close();
         d->m_udpSocket->close();
         d->_release_kcp();
-        d->m_updateTimer.stop();
-        disconnect(d->m_udpSocket, &QUdpSocket::readyRead, nullptr, nullptr);
 
         if (d->m_isConnceted) {
             d->m_isConnceted = false;
